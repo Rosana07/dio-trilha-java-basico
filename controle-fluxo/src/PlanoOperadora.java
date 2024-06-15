b574c92cef609b4eb18b3b9e4674bf3212c2ccac
Rosana07 <emaildonotreply>
2024-06-15 20:49:30 -0300

    Modo condicional switch/case

--- controle-fluxo/src/PlanoOperadora.java
+++ controle-fluxo/src/PlanoOperadora.java
@@ -1,6 +1,6 @@
 public class PlanoOperadora {
     public static void main(String[] args) {
-        String plano = "M"; //M / T
+        String plano = "T"; //M / T
 		
 		if(plano == "B") {
 			System.out.println("100 minutos de ligação");
