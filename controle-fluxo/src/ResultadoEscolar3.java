07d32e2c8954776c3e11eb4cd0aa48f0d697f029
Rosana07 <emaildonotreply>
2024-06-15 20:14:02 -0300

    condição ternaria tipo encadeada

--- controle-fluxo/src/ResultadoEscolar3.java
+++ controle-fluxo/src/ResultadoEscolar3.java
@@ -1,7 +1,7 @@
 public class ResultadoEscolar3 {
     public static void main(String[] args) {
-        int nota = 6;
-		String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+        int nota = 7;
+		String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
 		System.out.println(resultado);
     }
 }
