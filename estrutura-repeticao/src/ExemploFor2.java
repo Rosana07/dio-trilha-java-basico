57e05764c626b5b68ea15d1eba1f1908c26c7cfe
Rosana07 <emaildonotreply>
2024-06-15 23:17:56 -0300

    estrutura 2 FOR

---
+++ estrutura-repeticao/src/ExemploFor2.java
@@ -0,0 +1,9 @@
+public class ExemploFor2 {
+    public static void main(String[] args) {
+        int carneirinhos = 1;
+        for( ; carneirinhos <=20; ) {
+        System.out.println(carneirinhos + " - Carneirinho(s)");
+        carneirinhos ++;
+}
+    }
+}
