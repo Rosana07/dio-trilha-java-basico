cdb5dae41c0fe70083d5bfee133d3ee1e5b9de0c
Rosana07 <emaildonotreply>
2024-06-15 23:02:14 -0300

    estrutura 1 FOR

--- controle-fluxo/src/PlanoOperadora2.java
+++ controle-fluxo/src/PlanoOperadora2.java
@@ -1,6 +1,6 @@
 public class PlanoOperadora2 {
     public static void main(String[] args) {
-        String plano = "B"; // M / T
+        String plano = "M"; // M / T
 
 		switch (plano) {
 			case "T": {
